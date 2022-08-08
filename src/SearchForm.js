1c9dbf76e3951397a4c83e62b79a5bf4b84e992f
Sophie-CR <104981706+Sophie-CR@users.noreply.github.com>
2022-08-08 18:02:50 +0100

    Added search form

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -2,5 +2,34 @@ import React from "react";
 import "./SearchForm.css";
 
 export default function SearchForm() {
-  return <p>Search Form</p>;
+  return (
+    <form className="form container">
+      <div className="row location">
+        <div className="col-sm-4 location-element search">
+          <input
+            className="search-input form-control"
+            type="text"
+            aria-describedby="Enter Location"
+            placeholder="Enter a location"
+            autoFocus="on"
+          />
+        </div>
+        <div className="col-sm-2 location-element">
+          <input
+            type="submit"
+            className="btn btn-success shadow-sm search-btn"
+            value="🔍 Search"
+          />
+        </div>
+        <div className="col-sm-6 location-element">
+          <button
+            className="btn btn-primary shadow-sm use-location"
+            type="submit"
+          >
+            📍 Use My Location
+          </button>
+        </div>
+      </div>
+    </form>
+  );
 }
